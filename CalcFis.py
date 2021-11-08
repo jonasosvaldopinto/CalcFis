b3acd02827d2db453559e407bf8778a652bef61e
j0n4as0sv4ald0 <78802852+jonasosvaldopinto@users.noreply.github.com>
2021-11-08 09:59:59 -0300

    Add files via upload

---
+++ CalcFis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#medindo a força
+força = int(input("Força (Newtons) :"))
+#medindo o atrito
+atrito = int(input("Atrito (Newtons) :"))
+#medindo o deslocamento
+deslocamento = int(input("Deslocamento (Metros) :"))
+#medindo o trabalho
+Trabalho = força - atrito 
+trabalho_2 = Trabalho * deslocamento
+
+print("os joules correspondentes são :")
+print(Trabalho + trabalho_2 - 10)
+print("(Joules)")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
